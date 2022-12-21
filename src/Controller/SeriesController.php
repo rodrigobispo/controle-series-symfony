d5fc09f5d0380b55b8da2a9e04914735b7aef349
rodrigobispo <rodrigoaugustocb@gmail.com>
2022-12-21 17:55:23 -0300

    Add alteração e exclusão; rota por name e param; msg básica sem flashmessage; sobrescrita de method http

--- src/Controller/SeriesController.php
+++ src/Controller/SeriesController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Series;
 use App\Repository\SeriesRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,21 +13,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SeriesController extends AbstractController
 {
-    public function __construct(private SeriesRepository $seriesRepository)
+    public function __construct(private SeriesRepository $seriesRepository, private EntityManagerInterface $entityManager)
     {
     }
 
     #[Route('/series', name: 'app_series', methods: ['GET'])]
-    public function index(): Response
+    public function seriesList(Request $request): Response
     {
         $seriesList = $this->seriesRepository->findAll();
+        $session = $request->getSession();
+        $successMessage = $session->get(name: 'success');
+        $session->remove(name: 'success');
 
         return $this->render('series/index.html.twig', [
             'seriesList' => $seriesList,
+            'successMessage' => $successMessage,
         ]);
     }
 
-    #[Route('series/create', methods: ['GET'])]
+    #[Route('series/create', name: 'app_series_form', methods: ['GET'])]
     public function addSeriesForm(): Response
     {
         return $this->render('series/form.html.twig');
@@ -37,8 +42,37 @@ class SeriesController extends AbstractController
     {
         $seriesName = $request->get("name");
         $series = new Series($seriesName);
-        $this->seriesRepository->save($series, true);
+        $request->getSession()
+            ->set('success', "Série \"{$seriesName}\" adicionada com sucesso");
+
+        $this->seriesRepository->save($series, flush: true);
 
         return new RedirectResponse('/series');
+    }
+
+    #[Route('/series/delete/{id}', name: 'app_delete_series', methods: ['DELETE'])]
+    public function deleteSeries(int $id, Request $request): Response
+    {
+        $this->seriesRepository->removeById($id);
+        $session = $request->getSession();
+        $session->set('success', "Série removida com sucesso");
+
+        return new RedirectResponse(url: '/series');
+    }
+
+    #[Route('/series/edit/{series}', name: 'app_edit_series_form', methods: ['GET'])]
+    public function editSeriesForm(Series $series): Response
+    {
+        return $this->render('series/form.html.twig', compact(var_name: 'series'));
+    }
+
+    #[Route('/series/edit/{series}', name: 'app_store_series_changes', methods: ['PATCH'])]
+    public function storeSeriesChanges(Series $series, Request $request): Response
+    {
+        $series->setName($request->request->get('name'));
+        $request->getSession()->set('success', "Série \"{$series->getName()}\" editada com sucesso");
+        $this->entityManager->flush();
+
+        return new RedirectResponse(url: '/series');
     }
 }
